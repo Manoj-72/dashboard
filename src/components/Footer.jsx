724c57f0ab69259ca86fa30ea591aa7352c7c00e
ManojBlockX <manoj.c@blockxtechnologies.com>
2022-09-23 11:18:35 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-14">
+    <p className="dark:text-gray-200 text-gray-700 text-xs text-center m-5">
+      © 2022 All rights reserved by Ezio
+    </p>
+  </div>
+);
+
+export default Footer;
---
+++ src/components/index.jsx
@@ -0,0 +1,5 @@
+export {default as Header} from './Header';
+export {default as Navbar} from './Navbar';
+export {default as Sidebar} from './Sidebar';
+export {default as ThemeSettings} from './ThemeSettings';
+export {default as Footer} from './Footer';
